1aadd43a65bfeff2af29c1c6fb0551145a51a5dd
Dano Colombo <danocolombo@gmail.com>
2021-11-19 22:02:19 -0500

    makeStyle and basic themes in place

--- arcdevelopment/src/components/App.js
+++ arcdevelopment/src/components/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
+import { ThemeProvider } from '@material-ui/core/styles';
 import Header from './ui/header';
+import theme from './ui/theme';
 
 
 function App() {
   return (
-    <div className="App">
+    <ThemeProvider theme={theme}>
       <Header/>
-      Hello
-    </div>
+        Hello
+      </ThemeProvider>
   );
 }
 
---
+++ arcdevelopment/src/components/ui/theme.js
@@ -0,0 +1,24 @@
+import { createTheme } from '@material-ui/core/styles';
+
+//common colors used throughout
+const arcBlue = "#0B7289";
+const arcOrange = "#FFBA60";
+export default createTheme({
+    palette: {
+        common: {
+            blue: `${arcBlue}`,
+            orange: `${arcOrange}`
+        },
+        primary: {
+            main: `${arcBlue}`,
+        },
+        secondary: {
+            main: `${arcOrange}`
+        }
+    },
+    typography: {
+        h3: {
+            fontWeight: 300
+        }
+    }
+})
