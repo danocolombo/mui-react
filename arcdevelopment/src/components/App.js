525a3893ebe502a82dae9949f81eef664f2bb3a4
Dano Colombo <danocolombo@gmail.com>
2021-11-23 06:16:00 -0500

    changed router.dom and Routes back to Switch to match Udemy class

--- arcdevelopment/src/components/App.js
+++ arcdevelopment/src/components/App.js
@@ -2,14 +2,14 @@ import React from "react";
 import { ThemeProvider } from "@material-ui/core/styles";
 import Header from "./ui/header";
 import theme from "./ui/theme";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <BrowserRouter>
         <Header />
-        <Routes>
+        <Switch>
           <Route exact path="/" component={() => <div>HOME</div>}/>
           <Route exact path="/services" component={() => <div>Services</div>} />
           <Route exact path="/customsoftware" component={() => <div>Custom Software</div>} />
@@ -19,7 +19,7 @@ function App() {
           <Route exact path="/about" component={() => <div>About</div>} />
           <Route exact path="/contact" component={() => <div>Contact Us</div>} />
           
-        </Routes>
+        </Switch>
       </BrowserRouter>
     </ThemeProvider>
   );  
